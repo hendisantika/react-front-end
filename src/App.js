5ac97a8906b37febbed1af0902b9b0f42fb0d66f
hendisantika <hendisantika@yahoo.co.id>
2024-12-17 05:05:02 +0700

    Modify App.css n App.js 🫸🌀✏️📗🐧🐳⬆

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import CreateOrUpdateEmployee from './Components/CreateOrUpdateEmployee';
+import FooterComponent from './Components/FooterComponent';
+import HeaderComponent from './Components/HeaderComponent';
+import ListEmployee from './Components/ListEmployee';
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
+import ViewEmployee from './Components/ViewEmployee';
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <>
+            <Router>
+                <div className="container">
+                    <HeaderComponent/>
+                    <Routes>
+                        <Route exact path="/" element={<ListEmployee/>}/>
+                        <Route exact path="/add-employee/:id" element={<CreateOrUpdateEmployee/>}/>
+                        <Route exact path="/view-employee/:id" element={<ViewEmployee/>}/>
+                    </Routes>
+                    <FooterComponent/>
+                </div>
+            </Router>
+        </>
     );
 }
 
