1de0985493f6e0f905dbc49e50eb0a16535f8b7e
hendisantika <hendisantika@yahoo.co.id>
2024-12-17 05:06:14 +0700

    Add FooterComponent.jsx components 🫸🌀✏️📗🐧🐳⬆

---
+++ src/Components/FooterComponent.jsx
@@ -0,0 +1,8 @@
+const FooterComponent = () => {
+    return (
+        <footer className="footer bg-dark">
+            <span className="text-muted">All rights reserved to Hendi Santika</span>
+        </footer>
+    );
+}
+export default FooterComponent;
